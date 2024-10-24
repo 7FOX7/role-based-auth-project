a1f778442bec85c6569248ca3e36b92ee4758416
7FOX7 <kheeugene@gmail.com>
2024-10-24 12:48:29 -0400

    Added functionality for the Login form

--- app/actions/login-form-action.ts
+++ app/actions/login-form-action.ts
@@ -1,6 +1,59 @@
+"use server"
+
 import { z } from "zod";
+import * as argon2 from "argon2"
+import { PrismaClient } from "@prisma/client";
+import { redirect } from "next/navigation";
+import { State } from "../lib/definitions";
+
+const prisma = new PrismaClient(); 
 
 const FormSchema = z.object({
-   email: z.string().min(1, "Email field cannot be empty"),
-   password: z.string().min(1, "Password field cannot be empty"), 
+   email: z.string().min(1, "Email field cannot be empty").trim(),
+   password: z.string().min(1, "Password field cannot be empty").trim(), 
 })
+
+export default async function login(prevState: State, formData: FormData) {
+   const validatedEntries = FormSchema.safeParse({
+      email: formData.get('email'), 
+      password: formData.get('password')
+   })
+
+   if(!validatedEntries.success) {
+      return {
+         errors: validatedEntries.error.flatten().fieldErrors, 
+      }
+   }
+
+   const {email, password} = validatedEntries.data
+   
+   try {
+      const user = await prisma.user.findFirst({
+         where: {
+            email: {
+               startsWith: email
+            }
+         }
+      })
+      
+      if(!user) {
+         return {
+            message: 'User with the given email is not found.'
+         }
+      }
+
+      const passwordMatch = await argon2.verify(user.password, password)
+
+      if(!passwordMatch) {
+         return {
+            message: 'Invalid credentials'
+         }
+      }
+
+   }
+   catch(err) {
+      throw new Error('Server Error. Failed to perform operation with the database: ' + err)
+   }
+
+   redirect('/')
+}
--- app/ui/login/login-form.tsx
+++ app/ui/login/login-form.tsx
@@ -1,44 +1,74 @@
 'use client'
 
-import { FormEvent, useState } from "react"
+import { useState } from "react"
+import { useActionState } from "react"
 import { useRouter } from "next/navigation"
+import { State } from "@/app/lib/definitions"
+import login from "@/app/actions/login-form-action"
+
+const initialState: State = {
+   errors: {}, 
+   message: ''
+}
 
 export default function LoginForm() {
    const [emailValue, setEmailValue] = useState('')
+   const [state, formAction] = useActionState(login, initialState)
    const router = useRouter()
 
    function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
       const {value} = e.target;
-
       setEmailValue(value)
    }
 
-   function handleSubmit(e: FormEvent) {
-      e.preventDefault()
-   }
    return (
       <>
-         <form onSubmit={handleSubmit} className="flex flex-col">
+         <form action={formAction} className="flex flex-col">
             <input 
                name="email"
                className="mb-5" 
+               aria-labelledby="email-error"
                type="text" 
                placeholder="Email"
                value={emailValue}
                onChange={handleInput} 
             />
+            <div 
+               id="email-error" 
+               aria-live="polite" 
+               aria-atomic="true"
+            >
+               {state?.errors?.email?.map((error) => {
+                  return <p key={error} className="mt-2 text-sm text-red-500">{error}</p>
+               })}
+            </div>
             <input 
                name="password"
                className="mb-5" 
+               aria-labelledby="password-error"
                type="text" 
                placeholder="Password" 
             />
+            <div 
+               id="password-error" 
+               aria-live="polite" 
+               aria-atomic="true"
+            >
+               {state?.errors?.password?.map((error) => {
+                  return <p key={error} className="mt-2 text-sm text-red-500">{error}</p>
+               })}
+            </div>
             <button 
                className="bg-red-500" 
                type="submit"
             >
                Submit
             </button>
+            {state?.message && 
+               <div>
+                  <p className="mt-2 text-sm text-red-500">{state.message}</p>
+               </div>
+            }
          </form>
          <button onClick={() => router.replace('/registration')}>First time? Register instead</button>
       </>
