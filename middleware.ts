0fc03988b5dea6a59123aac3ac86610fc0177124
7FOX7 <kheeugene@gmail.com>
2024-10-26 03:13:32 -0400

    Adjusted middleware

--- middleware.ts
+++ middleware.ts
@@ -11,33 +11,32 @@ export default async function middleware(req: NextRequest) {
       const path = req.nextUrl.pathname; 
       const isPublicRoute = publicRoutes.includes(path)
       const isProtectedRoute = protectedRoutes.includes(path)
-
       const session = (await cookies()).get('session')?.value
       const decrypted = await decrypt(session)  // verify JWT is valid 
 
-      if(!session || !decrypted) {
-         return NextResponse.redirect(new URL('/login', req.nextUrl))
+      if(!isPublicRoute) {
+         if(!session || !decrypted) {
+            return NextResponse.redirect(new URL('/login', req.nextUrl))
+         }
       }
+      if(session && decrypted) {
+         const expires = new Date(Date.now() + 2 * 60 * 1000)
+         const cookieStorage = await cookies()
+         cookieStorage.set('session', session, {
+            expires, 
+            httpOnly: true
+         })
+         console.log('you are all good. you are registered')
 
-      const expires = new Date(Date.now() + 2 * 60 * 1000)
-      const cookieStorage = await cookies()
-      cookieStorage.set('session', session, {
-         expires, 
-         httpOnly: true
-      })
-      console.log('you are all good. you are registered')
+         if(path.startsWith('/control') && decrypted.payload.isAdmin === false) {
+            return NextResponse.redirect(new URL('/', req.nextUrl))
+         }
 
-      if(path.startsWith('/control') && decrypted.payload.isAdmin === false) {
-         console.log('you are not allowed to see this page. Redirecting')
-         return NextResponse.redirect(new URL('/', req.nextUrl))
+         if(isPublicRoute && session) {
+            return NextResponse.redirect(new URL('/', req.nextUrl))
+         }
       }
-
-      if(isPublicRoute && decrypted) {
-         console.log('I dont let you login twice')
-         return NextResponse.redirect(new URL('/', req.nextUrl))
-      }
-
-      return NextResponse.next()
+      // return NextResponse.next()
    }
    catch(err) {
       throw new Error('Could not update session: ' + err)
@@ -45,5 +44,5 @@ export default async function middleware(req: NextRequest) {
 }
 
 export const config = {
-   matcher: ['/', '/control'],
+   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 }
