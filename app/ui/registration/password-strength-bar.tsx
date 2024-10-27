e76c45dc907f4be96b92be057bd83660b66f7e4e
7FOX7 <kheeugene@gmail.com>
2024-10-26 20:38:16 -0400

    Adjusted the width property to be more responsive

--- app/ui/registration/password-strength-bar.tsx
+++ app/ui/registration/password-strength-bar.tsx
@@ -9,7 +9,7 @@ export default function PasswordStrengthBar({
    }) {
 
    return (
-      <div className="w-56">
+      <div className="w-65 md:w-56">
          <div className="bg-gray-300 w-full h-2 rounded-md mt-2">
             <div className={`${barColor} ${barWidth} h-full rounded-md`} />
          </div>
